8be3c1c9b8ac817b7cae1d0d9a9b941b65ed21b0
jdyang88 <64808996+jdyang88@users.noreply.github.com>
2024-11-17 07:08:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ PROJECTS = {
     "📚 Korea Annual Population Dashboard": "https://koreanpopulation-jdyang88.streamlit.app/",
     "📚 Korea Lotto Prediciton by 5 ML Models": "https://korealotto-jdyangh88.streamlit.app/",
     "📚 Korea Stocks Prediction by DL Model": "https://stocks-jdyang88.streamlit.app/",
-   # "📚 Korea Apartment Sales & Rent/Lease": "https://apartment-jdyang88.streamlit.app/",
+   "📚 Korea Apartment Sales & Rent/Lease": "https://apartment-jdyang88.streamlit.app/",
     "📚 Korea LNG Ltd. Homepage": "https://kolnghomepage.netlify.app/",
 }
 
