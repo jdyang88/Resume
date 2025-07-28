42ed69d2fb988e5952d7bbc66f8211e4030b2cef
jdyang88 <64808996+jdyang88@users.noreply.github.com>
2025-07-28 19:55:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ NAME = "JD Yang"
 DESCRIPTION = """
 Project Manager in SK earthon
 """
-EMAIL = "jdyang@sk.com" / "jdyang@kolng.com"
+EMAIL = "jdyang@sk.com or jdyang@kolng.com"
 
 # SOCIAL_MEDIA = {
 #     "YouTube": "https://youtube.com/c/codingisfun",
